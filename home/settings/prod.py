1e7e2e7769a77273fd5be0ecd547039ed5affe62
rakib06 <six.rakib@gmail.com>
2020-05-10 21:58:42 +0600

    :)

--- home/settings/prod.py
+++ home/settings/prod.py
@@ -1,19 +1,17 @@
 '''Use this for production'''
 
 from .base import *
+import dj_database_url
 
 DEBUG = False
 ALLOWED_HOSTS += ['https://rk-mcq.herokuapp.com']
 WSGI_APPLICATION = 'home.wsgi.prod.application'
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'db_name',
-        'USER': 'db_user',
-        'PASSWORD': 'db_password',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
